89a30bf19ad2ae16a94b3003dd9bb91cc1718780
Nikita Beletskiy <bel.nikita3197@gmail.com>
2021-02-05 14:30:25 +0300

    useState hook implementation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Navbar from './Navbar';
+import Home from './Home';
+
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Navbar />
+      <div className='content'>
+        <Home />
+      </div>
+    </div>
+  );
 };
 
 export default App;
