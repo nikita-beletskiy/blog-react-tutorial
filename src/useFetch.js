88cac0c5ff180f2fe9d055f22d846ee916980515
Nikita Beletskiy <bel.nikita3197@gmail.com>
2021-02-06 16:25:43 +0300

    Simple React Router implementation with Route Parameters and useEffect cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = url => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortController = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortController.signal })
         .then(res => {
           if (!res.ok) throw Error('error connecting to resourse');
           return res.json();
@@ -18,10 +20,15 @@ const useFetch = url => {
           setError(null);
         })
         .catch(err => {
-          setIsPending(false);
-          setError(err.message);
+          if (err.name === 'AbortError') console.log('fetch aborted');
+          else {
+            setIsPending(false);
+            setError(err.message);
+          }
         });
     }, 1000);
+
+    return () => abortController.abort();
   }, [url]);
 
   return { data, isPending, error };
